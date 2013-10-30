3cf0b0d1b29a18910016d3056e2faf37fcfa7289
evilone79 <evilone79@gmail.com>
2013-10-30 23:04:39 +0200

    types moving

---
+++ ObjectWalker/IObjectWalker.cs
@@ -0,0 +1,11 @@
+﻿namespace ObjectWalker
+{
+    public interface IObjectWalker
+    {
+        void WalkDown(string text);
+        void WalkLevel(string text);
+        void WalkUp();
+    }
+
+    
+}
