a79f91053339f82660b66131c11ccafe4be0bff0
evilone79 <evilone79@gmail.com>
2013-11-03 23:34:32 +0200

    XmlWalker added. TBF

---
+++ ObjectWalker/XmlWalker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ObjectWalker
+{
+    public class XmlWalker : IObjectWalker
+    {
+        private XDocument m_xml;
+        private Stack<XElement> m_stack;
+        private XElement m_curNode;
+ 
+        public XmlWalker(XDocument xml)
+        {
+            m_xml = xml;
+            m_curNode = new XElement("");
+            m_xml.Add(m_curNode);
+        }
+
+        public void WalkDown(string text)
+        {
+            m_stack.Push(m_curNode);
+            var newNode = new XElement(text);
+            m_curNode.Add(newNode);
+            m_curNode = newNode;
+        }
+
+        public void WalkLevel(string text)
+        {
+            m_curNode.Value = text;
+        }
+
+        public void WalkUp()
+        {
+            m_curNode = m_stack.Pop();
+        }
+    }
+}
